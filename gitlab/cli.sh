74de91e3adbd17992e8202149da1238318020f85
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-28 21:49:24 +0100

    update gitlab name to magic-gitlab

--- gitlab/cli.sh
+++ gitlab/cli.sh
@@ -11,7 +11,7 @@ function build() {
   echo "build finished"
 }
 
-function run() {
+function run-orig() {
   rm
 
   echo "run ${CONTAINER_NAME}"
@@ -20,7 +20,7 @@ function run() {
     --publish 0.0.0.0:${CONTAINER_PORT_443}:${HOST_PORT_443} \
     --publish 0.0.0.0:${CONTAINER_PORT_80}:${HOST_PORT_80} \
     --publish 0.0.0.0:${CONTAINER_PORT_22}:${HOST_PORT_22} \
-    --name gitlab \
+    --name ${CONTAINER_NAME} \
     --restart always \
     --volume config:/etc/gitlab \
     --volume logs:/var/log/gitlab \
@@ -30,18 +30,25 @@ function run() {
   echo "started docker container"
 }
 
-function run-test() {
+function run() {
+  rm
+
+  echo "starting container ${CONTAINER_NAME}"
+
   docker run \
     --hostname ${HOSTNAME} \
-    --name gitlab \
+    --name ${CONTAINER_NAME} \
     --detach \
     --link magic-postgres:postgresql \
     --link magic-redis:redisio \
     --publish $HOST_PORT_22:$CONTAINER_PORT_22 \
     --publish $HOST_PORT_80:$CONTAINER_PORT_80 \
-    --env 'GITLAB_PORT=$HOST_PORT_80' \
-    --env 'GITLAB_SSH_PORT=$HOST_PORT_22' \
-    --env 'GITLAB_SECRETS_DB_KEY_BASE=long-and-random-alpha-numeric-string-232323' \
+    --env "GITLAB_PORT=$HOST_PORT_80" \
+    --env "GITLAB_SSH_PORT=$HOST_PORT_22" \
+    --env "DB_NAME=$GITLAB_DB_NAME" \
+    --env "DB_USER=$GITLAB_DB_USER" \
+    --env "DB_PASS=$GITLAB_DB_PASS" \
+    --env "GITLAB_SECRETS_DB_KEY_BASE=long-and-random-alpha-numeric-string-232323" \
     --volume ${PWD}/data:/home/git/data \
     sameersbn/gitlab:8.4.1
 }
